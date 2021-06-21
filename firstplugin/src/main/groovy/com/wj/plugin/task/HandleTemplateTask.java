ead6417d751edd4d3c49d933586012ed4c428543
wenjing.liu <liuwenjing.12@163.com>
2021-06-21 13:18:53 +0800

    build ok

--- firstplugin/src/main/groovy/com/wj/plugin/task/HandleTemplateTask.java
+++ firstplugin/src/main/groovy/com/wj/plugin/task/HandleTemplateTask.java
@@ -12,42 +12,40 @@ import java.io.File;
 /**
  * Created by wenjing.liu on 2021/6/9 in J1.
  * <p>
- * 一个Task
- * <p>
- * TODO 暂时发现只能放到该目录下，其他的module才可以引用该Task
+ * 一个Task:实现一个定义的接口类来实现对应的实现类的功能
+ * 分析：
+ * input：一个接口文件
+ * output：一个实现类
+ * action：实现具体的实现功能
  *
  * @author wenjing.liu
  */
 public class HandleTemplateTask extends DefaultTask {
     private String compileSdk;
-    private File inputFile;
-    //private TemplateEngine
+    private File interfaceSourceDir;
 
 
-    public String getCompileSdk() {
-        SystemOutPrint.println(" compileSdk = " + compileSdk);
-        return compileSdk;
-    }
+//    public String getCompileSdk() {
+//        SystemOutPrint.println(" compileSdk = " + compileSdk);
+//        return compileSdk;
+//    }
 
     /**
-     * 必须有setter方法
+     * 必须有setter方法,否则在build.gradle配置该项的时候会识别不到
      */
     @Input
     public void setCompileSdk(String sdk) {
         this.compileSdk = sdk;
     }
 
-    // @InputFile
-    public File getInputFile() {
-        SystemOutPrint.println(" inputFile = " + inputFile.getPath());
-        return inputFile;
-    }
 
-
-    //@InputFile
-    public void taskInput(File file) {
-        this.getInputs().file(file);
-    }
+//    public void setInterfaceSourceDir(File interfaceSourceDir) {
+//        this.interfaceSourceDir = interfaceSourceDir;
+//    }
+//
+//    public File getInterfaceSourceDir() {
+//        return interfaceSourceDir;
+//    }
 
     @TaskAction
     public void taskAction() {
