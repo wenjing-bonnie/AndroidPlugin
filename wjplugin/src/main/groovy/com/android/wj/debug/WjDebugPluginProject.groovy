1b38292568835a37febbf67897473c7c298611c5
wenjing.liu <liuwenjing.12@163.com>
2021-07-19 17:22:10 +0800

    debug plugins

---
+++ wjplugin/src/main/groovy/com/android/wj/debug/WjDebugPluginProject.groovy
@@ -0,0 +1,11 @@
+package com.android.wj.debug
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+class WjDebugPluginProject implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
