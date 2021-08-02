b62d0c0d1daf06e1d7db442b593c3da173a86bab
wenjing.liu <liuwenjing.12@163.com>
2021-08-02 11:35:10 +0800

    add productFlavor

--- app/src/main/java/com/android/androidplugin/MainActivity.java
+++ app/src/main/java/com/android/androidplugin/MainActivity.java
@@ -1,7 +1,10 @@
 package com.android.androidplugin;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(getClass().getSimpleName(), "channel = " + getChannelFromAndroidMainfest());
+    }
+
+    private String getChannelFromAndroidMainfest() {
+        String metaChannel = "";
+        try {
+            ApplicationInfo info = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            if (info == null || info.metaData == null) {
+                return metaChannel;
+            }
+            metaChannel = info.metaData.getString("channel");
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        return metaChannel;
     }
 }
