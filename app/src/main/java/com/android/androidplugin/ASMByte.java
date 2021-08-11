c8448497f6d2a9528f1c1c9afd1704c7fdf93d4d
wenjing.liu <liuwenjing.12@163.com>
2021-08-11 17:23:33 +0800

    add cost time

--- app/src/main/java/com/android/androidplugin/ASMByte.java
+++ app/src/main/java/com/android/androidplugin/ASMByte.java
@@ -1,5 +1,6 @@
 package com.android.androidplugin;
 
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -12,8 +13,15 @@ import android.view.View;
 public class ASMByte implements View.OnClickListener {
     private int a = 10;
     private static int c = 20;
+    private String name;
 
     private int sum(int aa, int bb) {
+        long beginTime = System.currentTimeMillis();
+        System.out.println("Other running code");
+        long callTime = System.currentTimeMillis() - beginTime;
+        // Log.d(getClass().getSimpleName(), String.format("%s cost time is [%l]ms", name, callTime));
+        Log.d("AUTO", String.format("cost time is [%l]ms", callTime));
+
         return aa + bb;
     }
 
--- wjplugin/src/main/groovy/com/android/wj/debug/autologvisitor/AutoLogAdviceAdapter.java
+++ wjplugin/src/main/groovy/com/android/wj/debug/autologvisitor/AutoLogAdviceAdapter.java
@@ -1,5 +1,6 @@
 package com.android.wj.debug.autologvisitor;
 
+import com.android.tools.r8.v.b.L;
 import com.android.wj.debug.utils.SystemOutPrintln;
 
 import org.objectweb.asm.Label;
@@ -7,6 +8,7 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.AdviceAdapter;
+import org.w3c.dom.ls.LSLoadEvent;
 
 /**
  * Created by wenjing.liu on 2021/8/3 in J1.
@@ -16,6 +18,15 @@ import org.objectweb.asm.commons.AdviceAdapter;
  */
 public class AutoLogAdviceAdapter extends AdviceAdapter {
     private MethodVisitor methodVisitor;
+    private String name;
+    /**
+     * 该方法的参数
+     */
+    private Type[] args;
+    /**
+     * 是否开启统计调用时间
+     */
+    private boolean isInjectCallTime = true;
 
     /**
      * Constructs a new {@link AdviceAdapter}.
@@ -29,7 +40,18 @@ public class AutoLogAdviceAdapter extends AdviceAdapter {
      */
     protected AutoLogAdviceAdapter(int api, MethodVisitor methodVisitor, int access, String name, String descriptor) {
         super(api, methodVisitor, access, name, descriptor);
+        this.name = name;
         this.methodVisitor = methodVisitor;
+        this.args = Type.getArgumentTypes(descriptor);
+        for (Type type : args) {
+            SystemOutPrintln.println("internal name = " + type.getInternalName() + " , classname = " + type.getClassName());
+        }
+    }
+
+    @Override
+    public void visitParameter(String name, int access) {
+        super.visitParameter(name, access);
+        SystemOutPrintln.println(" = visitParameter name = " + name);
     }
 
     /**
@@ -49,6 +71,13 @@ public class AutoLogAdviceAdapter extends AdviceAdapter {
         super.onMethodEnter();
         SystemOutPrintln.println(" = onMethodEnter");
         //methodVisitor.
+        if (!isInjectCallTime()) {
+            return;
+        }
+        //添加方法调用前的时间,对应代码:long beginTime = System.currentTimeMillis();
+        methodVisitor.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
+        methodVisitor.visitVarInsn(Opcodes.LSTORE, 3);
+
     }
 
     /**
@@ -59,13 +88,96 @@ public class AutoLogAdviceAdapter extends AdviceAdapter {
     @Override
     protected void onMethodExit(int opcode) {
         super.onMethodExit(opcode);
-        SystemOutPrintln.println(" = onMethodExit");
+
     }
+
+    /**
+     * 之所以不在{@link #onMethodExit(int)}中,
+     * 是因为如果源码的return带有运算表达式,会先调用{@link #onMethodExit(int)},然后在计算表达式,最后才调用{@link #visitInsn(int)}
+     *
+     * @param opcode
+     */
+    @Override
+    public void visitInsn(int opcode) {
+        if (!isInjectCallTime()) {
+            super.visitInsn(opcode);
+            return;
+        }
+        //方法返回的时候,添加执行时间
+        if (opcode >= IRETURN && opcode <= RETURN) {
+            SystemOutPrintln.println(" = onMethodExit");
+            //添加方法调用之后的时间,对应代码:long callTime = System.currentTimeMillis() - beginTime;
+            methodVisitor.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
+            methodVisitor.visitVarInsn(LLOAD, 3);
+            methodVisitor.visitInsn(LSUB);
+            methodVisitor.visitVarInsn(LSTORE, 5);
+
+            //输出方法执行时间,对应代码:Log.d("AUTO", String.valueOf(callTime));
+            //logMethodExit();
+            logStringMethodExit();
+
+            // visitLocalVariable()描述或定义存储在Code属性的LocalVariableTable和LocalVariableTypeTable属性中的调试信息。 它们不是正常操作所必需的，与StackMapTable存储的信息不同。
+            // 换句话说，除非您想提供调试信息，否则无论是否自动计算堆栈映射帧，您都不需要调用visitLocalVariable() 。
+            //请注意这些属性中存储的信息的差异。 LocalVariable[Type]Table存储有关源级语言的局部变量的名称和[泛型]类型及其范围。 StackMapTable存储有关字节码验证器的JVM类型系统的局部变量和操作数堆栈条目的类型信息。
+            //methodVisitor.visitLocalVariable();
+        }
+        super.visitInsn(opcode);
+    }
+
+    @Override
+    public void visitLabel(Label label) {
+        super.visitLabel(label);
+        //label的作用是为了条件跳转，其实也可以理解成字节码指令的参数。所以label必须对应一条字节码指令，通过visitLabel(label)来调用，并且visitLabel的调用必须紧跟随着label对象指定的指令
+    }
+
+//    @Override
+//    public void visitMaxs(int maxStack, int maxLocals) {
+//        super.visitMaxs(maxStack + 3, maxLocals + 4);
+//    }
 
     @Override
     public void visitEnd() {
         super.visitEnd();
         SystemOutPrintln.println(" = visitEnd");
+    }
+
+    /**
+     * 输出方法执行时间,对应代码:Log.d("AUTO", String.valueOf(callTime));
+     */
+    private void logMethodExit() {
+        //输出方法执行时间,对应代码:Log.d("AUTO", String.valueOf(callTime));
+        methodVisitor.visitLdcInsn("AUTO");
+        methodVisitor.visitVarInsn(LLOAD, 5);
+        methodVisitor.visitMethodInsn(INVOKESTATIC, "java/lang/String", "valueOf", "(J)Ljava/lang/String;", false);
+        methodVisitor.visitMethodInsn(INVOKESTATIC, "android/util/Log", "d", "(Ljava/lang/String;Ljava/lang/String;)I", false);
+        methodVisitor.visitInsn(POP);
+    }
+
+    /**
+     * 输出方法执行时间,对应代码:Log.d("AUTO", String.format("cost time is [%l]ms", callTime));
+     */
+    private void logStringMethodExit() {
+        methodVisitor.visitLdcInsn("AUTO");
+        methodVisitor.visitLdcInsn("cost time is [%l] ms");
+        methodVisitor.visitInsn(ICONST_1);
+        methodVisitor.visitTypeInsn(ANEWARRAY, "java/lang/Object");
+        methodVisitor.visitInsn(DUP);
+        methodVisitor.visitInsn(ICONST_0);
+        methodVisitor.visitVarInsn(LLOAD, 5);
+        methodVisitor.visitMethodInsn(INVOKESTATIC, "java/lang/Long", "valueOf", "(J)Ljava/lang/Long;", false);
+        methodVisitor.visitInsn(AASTORE);
+        methodVisitor.visitMethodInsn(INVOKESTATIC, "java/lang/String", "valueOf", "(J)Ljava/lang/String;", false);
+        methodVisitor.visitMethodInsn(INVOKESTATIC, "android/util/Log", "d", "(Ljava/lang/String;Ljava/lang/String;)I", false);
+        methodVisitor.visitInsn(POP);
+    }
+
+    /**
+     * 需要增加方法执行时间的标识
+     *
+     * @return
+     */
+    private boolean isInjectCallTime() {
+        return !isInjectCallTime || !"<clinit>".equals(name);
     }
 
 }
