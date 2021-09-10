b1e72c787f24b41c10afebdbd4fbb8651ecf982e
wenjing.liu <liuwenjing.12@163.com>
2021-09-10 16:58:11 +0800

    add export for manifest plugin

---
+++ manifestplugin/src/main/groovy/com/wj/manifest/AddExportForManifestTask.java
@@ -0,0 +1,20 @@
+package com.wj.manifest;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+/**
+ * Created by wenjing.liu on 2021/9/9 in J1.
+ * <p>
+ * 适配Android12,为每个带有<intent-filter>添加android:exported="true"属性
+ *
+ * @author wenjing.liu
+ */
+public class AddExportForManifestTask extends DefaultTask {
+    protected static final String TAG = "AddExportForManifestTask";
+
+    @TaskAction
+    public void run() {
+        SystemOutPrint.println(" AddExportForManifestTask is running !");
+    }
+}
--- manifestplugin/src/main/groovy/com/wj/manifest/ManifestProject.groovy
+++ manifestplugin/src/main/groovy/com/wj/manifest/ManifestProject.groovy
@@ -1,19 +1,38 @@
 package com.wj.manifest
 
+import com.android.build.gradle.AppExtension
 import com.android.build.gradle.BaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 
 /**
  * 插件入口
  */
 class ManifestProject implements Plugin<Project> {
+    List variantManifestTaskNames = new ArrayList()
 
     @Override
     void apply(Project project) {
         SystemOutPrint.println("Welcome ManifestProject")
-        project.extensions.findByType(BaseExtension.class).variantFilter {
-            SystemOutPrint.println(it.name)
+        addExportTaskForManifest(project)
+        project.extensions.findByType(AppExtension.class).variantFilter {
+            SystemOutPrint.println("variant = " + it.name)
+            variantManifestTaskNames.add(String.format("process%sManifest", it.name))
+        }
+    }
+
+    void addExportTaskForManifest(Project project) {
+        Task task = project.getTasks().create(AddExportForManifestTask.TAG, AddExportForManifestTask)
+        project.afterEvaluate {
+            project.getTasks().matching {
+                for (int i = 0; i < variantManifestTaskNames.size(); i++) {
+                    it.name.equals(variantManifestTaskNames.get(i))
+                }
+            }.each {
+                SystemOutPrint.println(" == afterEvaluate == " + it.name)
+                it.dependsOn(task)
+            }
         }
     }
 
