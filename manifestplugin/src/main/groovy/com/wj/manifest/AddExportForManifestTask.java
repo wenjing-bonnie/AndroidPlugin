d1fa89581dad8575efd5f92725c6ff53f942401a
wenjing.liu <liuwenjing.12@163.com>
2021-09-15 11:54:40 +0800

    add manifest

--- manifestplugin/src/main/groovy/com/wj/manifest/AddExportForManifestTask.java
+++ manifestplugin/src/main/groovy/com/wj/manifest/AddExportForManifestTask.java
@@ -1,7 +1,13 @@
 package com.wj.manifest;
 
+import com.android.build.gradle.tasks.ProcessMultiApkApplicationManifest;
+
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wenjing.liu on 2021/9/9 in J1.
@@ -11,10 +17,57 @@ import org.gradle.api.tasks.TaskAction;
  * @author wenjing.liu
  */
 public class AddExportForManifestTask extends DefaultTask {
+    private String manifestFilePath;
+    private List variantNames = new ArrayList<String>();
     protected static final String TAG = "AddExportForManifestTask";
+
+    /**
+     * 设置Manifest文件的路径
+     *
+     * @param path 如Users/j1/Documents/android/code/studio/AndroidPlugin/app/build/intermediates/merged_manifest/xiaomiRelease/AndroidManifest.xml
+     */
+    public void setManifestFilePath(String path) {
+        this.manifestFilePath = path;
+    }
+
+    /**
+     * 设置所有的变体名称
+     *
+     * @param names
+     */
+    public void setVariantNames(List names) {
+        this.variantNames = names;
+    }
 
     @TaskAction
     public void run() {
         SystemPrint.outPrintln(" AddExportForManifestTask is running !");
+        handlerVariantsManifestFile();
     }
+
+    /**
+     * 处理所有变体的AndroidManifest文件
+     */
+    private void handlerVariantsManifestFile() {
+        int size = variantNames.size();
+        for (int i = 0; i < size; i++) {
+            String deleteManifest = manifestFilePath.substring(0, manifestFilePath.lastIndexOf("/"));
+            String newFilePath = String.format("%s/%s/AndroidManifest.xml",
+                    deleteManifest.substring(0, deleteManifest.lastIndexOf("/")), variantNames.get(i));
+            File manifestFile = new File(newFilePath);
+            handlerVariantManifestFile(manifestFile);
+        }
+    }
+
+    /**
+     * 处理单个变体的Manifest文件
+     */
+    private void handlerVariantManifestFile(File manifestFile) {
+        if (!manifestFile.exists()) {
+            return;
+        }
+        SystemPrint.outPrintln("正在处理\n " + manifestFile.getAbsolutePath());
+        
+    }
+
 }
--- manifestplugin/src/main/groovy/com/wj/manifest/ManifestProject.groovy
+++ manifestplugin/src/main/groovy/com/wj/manifest/ManifestProject.groovy
@@ -1,15 +1,15 @@
 package com.wj.manifest
 
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.tasks.ProcessMultiApkApplicationManifest
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 
 /**
  * 插件入口
  */
 class ManifestProject implements Plugin<Project> {
-    List variantManifestTaskNames = new ArrayList()
+    List variantNames = new ArrayList()
 
     @Override
     void apply(Project project) {
@@ -23,46 +23,56 @@ class ManifestProject implements Plugin<Project> {
      * @param project
      */
     void getAllVariantManifestTask(Project project) {
-        project.extensions.findByType(AppExtension.class).variantFilter {
-            variantManifestTaskNames.add(String.format("process%sManifest", it.name.capitalize()))
-        }
+        project.extensions.findByType(AppExtension.class)
+                .variantFilter {
+                    variantNames.add(it.name)
+                }
     }
     /**
      * 将添加android:exported的task添加到任务队列中
      * @param project
      */
     void addExportTaskForManifest(Project project) {
-        Task task = project.getTasks().create(AddExportForManifestTask.TAG, AddExportForManifestTask)
+        AddExportForManifestTask addTask = project.getTasks().create(AddExportForManifestTask.TAG, AddExportForManifestTask)
+        addTask.setVariantNames(variantNames)
+        //在项目配置完成后,添加自定义Task
         project.afterEvaluate {
+            //方案一:直接通过task的名字找到ProcessApplicationManifest这个task
+            variantNames.each {
+                //直接找到ProcessApplicationManifest，然后在执行后之后执行该Task
+                ProcessMultiApkApplicationManifest processManifestTask = project.getTasks().getByName(String.format("process%sManifest", it.capitalize()))
+                addTask.setManifestFilePath(processManifestTask.getMainMergedManifest().get().getAsFile().getAbsolutePath())
+                processManifestTask.finalizedBy(addTask)
+            }
+            //方案二:从project中的所有task进行匹配
             project.getTasks().each {
                 //在process%sManifest任务之后
                 if (it.name.startsWith("process") && it.name.endsWith("Manifest")) {
-                    for (int i = 0; i < variantManifestTaskNames.size(); i++) {
-                        if (it.name.equals(variantManifestTaskNames.get(i))) {
+                    for (int i = 0; i < variantNames.size(); i++) {
+                        if (it.name.equals(variantNames.get(i))) {
+
                             //在process%sManifest添加解析xml文件的task
                             //Circular dependency between the following tasks:
-                            //it.dependsOn(task)
+                            //it.dependsOn(addTask)
                             //mustRunAfter并不会添加依赖，只是高度Gradle执行的优先级
                             //无论是shouldRunAfter还是mustRunAfter，影响的只是task在队列中的顺序，并不影响任何任务间的执行依赖，也就是说使用shouldRunAfter和mustRunAfter并不会导致任务队列中添加新的task。
-                            //task.mustRunAfter(it)
-                            SystemPrint.errorPrintln("进入了 process Manifest")
+                            //addTask.mustRunAfter(it)
+                            //SystemPrint.errorPrintln("进入了 process Manifest")
                             //TODO 这种方式只能添加在这个process%sManifest之前
-                             //it.dependsOn(task)
+                            //it.dependsOn(addTask)
                             //现在还行task1 在执行task2
                             //task2.dependsOn(task1)
                             //用于任务执行结束后自动执行其他任务。taskC.finalizedBy taskD 执行完taskC后执行taskD
-                            it.finalizedBy(task)
-//                            it.doLast {
-//                                SystemPrint.errorPrintln(it.name + " 执行完毕 ")
-//                            }
+                            //it.finalizedBy(addTask)
+
                         }
                     }
                 }
                 //引用第三方sdk里面有一些多申请的权限，我们也可以在这边直接剔除掉，这里选择的切入点在processResources的任务执行之前，
-                //在processResources任务之前，这个发现只有在非app module中才有这个processResources task
+                //在processResources任务之前，这个发现只有在非app module中才有这个processResources addTask
 //                if (it.name.startsWith("processResources")) {
 //                    SystemPrint.errorPrintln("进入了 processResources")
-//                    it.dependsOn(task)
+//                    it.dependsOn(addTask)
 //                }
             }
         }
