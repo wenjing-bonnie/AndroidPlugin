e7db54988bab2bc890287f6db0efe4d95603469d
wenjing.liu <liuwenjing.12@163.com>
2021-09-16 11:33:20 +0800

    add exported for activity

--- manifestplugin/src/main/groovy/com/wj/manifest/AddExportForManifestTask.groovy
+++ manifestplugin/src/main/groovy/com/wj/manifest/AddExportForManifestTask.groovy
@@ -1,6 +1,6 @@
 package com.wj.manifest
 
-
+import groovy.xml.XmlUtil
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.xml.sax.SAXException
@@ -14,18 +14,18 @@ import javax.xml.parsers.ParserConfigurationException
  *
  * @author wenjing.liu
  */
-public class AddExportForManifestTask extends DefaultTask {
+class AddExportForManifestTask extends DefaultTask {
     private String manifestFilePath;
     private List variantNames = new ArrayList<String>();
     protected static final String TAG = "AddExportForManifestTask";
-    //private final String ATTRIBUTE_EXPORT = "{http://schemas.android.com/apk/res/android}exported"
+    String ATTRIBUTE_EXPORT = "{http://schemas.android.com/apk/res/android}exported"
 
     /**
      * 设置Manifest文件的路径
      *
      * @param path 如Users/j1/Documents/android/code/studio/AndroidPlugin/app/build/intermediates/merged_manifest/xiaomiRelease/AndroidManifest.xml
      */
-    public void setManifestFilePath(String path) {
+    void setManifestFilePath(String path) {
         this.manifestFilePath = path;
     }
 
@@ -34,20 +34,14 @@ public class AddExportForManifestTask extends DefaultTask {
      *
      * @param names
      */
-    public void setVariantNames(List names) {
+    void setVariantNames(List names) {
         this.variantNames = names;
     }
 
     @TaskAction
-    public void run() {
+    void run() {
         SystemPrint.outPrintln(" AddExportForManifestTask is running !");
-        handlerVariantsManifestFile();
-    }
-
-    /**
-     * 处理所有变体的AndroidManifest文件
-     */
-    private void handlerVariantsManifestFile() {
+        //处理所有变体的AndroidManifest文件
         int size = variantNames.size();
         for (int i = 0; i < size; i++) {
             String deleteManifest = manifestFilePath.substring(0, manifestFilePath.lastIndexOf("/"));
@@ -63,7 +57,7 @@ public class AddExportForManifestTask extends DefaultTask {
      */
     private void handlerVariantManifestFile(File manifestFile) {
         if (!manifestFile.exists()) {
-            return;
+            return
         }
         SystemPrint.outPrintln("正在处理\n " + manifestFile.getAbsolutePath());
         try {
@@ -74,22 +68,19 @@ public class AddExportForManifestTask extends DefaultTask {
             //node.attributes();获取的一级内容<?xml> <manifest>里设置的内容如:key为package、encoding,value为对应的值
             //node.children();获取的二级内容 <application> <uses-sdk>
             //node.application直接可获取到<application>这级标签
+            //第一步:处理<activity>
             node.application.activity.each {
-                //attributes()取得是在<activity >里面配置的属性值,而里面嵌套的<></>可直接通过.xxx的形式取得
-                def attrs = it.attributes()
-                //如果含有了android:exported,则直接返回.
-                boolean isExported = hasAttributeExported(attrs);
-                if (hasAttributeExported(attrs)) {
-                    SystemPrint.outPrintln("已经含有了android:exported,直接返回")
-                    //结束本次循环,相当于continue find return true相当于break
+                SystemPrint.outPrintln("The idada is = " + it)
+                //如果已经有android:exported,则直接循环下一个:return true 相当于continue
+                if (handlerEveryNodeWithoutExported(it)) {
                     return true
                 }
-                //得到配置的<activity>里面的如<intent-filter>
-                def children = it.children()
-                if (hasIntentFilter(children)) {
-                    handlerAddExportForActivity(it)
-                }
             }
+            //第二步:处理<service>
+            //第三步:处理<receiver>
+            //第四步:保存到原AndroidManifest文件中
+            String result = XmlUtil.serialize(node)
+            manifestFile.write(result, "utf-8")
         } catch (ParserConfigurationException e) {
             e.printStackTrace()
         } catch (SAXException e) {
@@ -99,23 +90,48 @@ public class AddExportForManifestTask extends DefaultTask {
         }
     }
     /**
+     * 为每个需要添加android:exported的node
+     * 在each{}中该方法不能定义为private,否则会提示找到该方法
+     * @param it
+     * @return
+     */
+    boolean handlerEveryNodeWithoutExported(Node it) {
+        //attributes()取得是在<activity >里面配置的属性值,而里面嵌套的<></>可直接通过.xxx的形式取得
+        def attrs = it.attributes()
+        //如果含有了android:exported,则直接处理下一个.
+        if (hasAttributeExported(attrs)) {
+            SystemPrint.errorPrintln("已经含有了\" android:exported \",可以处理下一个了")
+            //结束本次循环,相当于continue find return true相当于break
+            return true
+        }
+        //得到配置的<activity>里面的如<intent-filter>
+        def children = it.children()
+        if (hasIntentFilter(children)) {
+            SystemPrint.outPrintln(String.format("因为处理的第三方SDK,所以都添加\" android:exported=true \""))
+            handlerAddExportForNode(it)
+        }
+        return false
+    }
+    /**
      * 添加android:export
      */
-    void handlerAddExportForActivity(Node activity) {
-        SystemPrint.outPrintln(String.format("开始添加android:exported"))
-        SystemPrint.errorPrintln("activity it = " + activity)
-
+    private void handlerAddExportForNode(Node node) {
+        SystemPrint.outPrintln("The node is = " + node)
+        SystemPrint.outPrintln(" ==== ")
+        //putAt()只能给已有的属性进行修改value
+        node.attributes().replace("android:name","1")
+        //注意这里使用的是"android:exported"而不是ATTRIBUTE_EXPORT!!!!!!
+        node.attributes().put("android:exported", true)
     }
     /**
      * 是否含有android:exported属性
      * TODO attrs.containsKey(ATTRIBUTE_EXPORT) 不起作用
      * @return
      */
-    boolean hasAttributeExported(Map attrs) {
-        String ATTRIBUTE_EXPORT = "{http://schemas.android.com/apk/res/android}exported"
+    private boolean hasAttributeExported(Map attrs) {
         boolean isExported = false
         attrs.find {
-            SystemPrint.outPrintln("hasAttributeExported key = " + it.key + " , value = " + it.value)
+            //SystemPrint.outPrintln("hasAttributeExported key = " + it.key + " , value = " + it.value)
             if (ATTRIBUTE_EXPORT.equals(it.key.toString())) {
                 isExported = true
                 //find return true相当于break
@@ -129,10 +145,10 @@ public class AddExportForManifestTask extends DefaultTask {
      * @param children
      * @return
      */
-    boolean hasIntentFilter(List children) {
+    private boolean hasIntentFilter(List children) {
         boolean isIntent = false
         children.find {
-            SystemPrint.errorPrintln("hasIntentFilter children name = " + it.name())
+            //SystemPrint.errorPrintln("hasIntentFilter children name = " + it.name())
             if ("intent-filter".equals(it.name())) {
                 isIntent = true
                 //find return true相当于break
